8c1ba707dcbc21d513bad1bae6126c3d540ed522
aike1202 <2433442840@qq.com>
2026-06-16 15:10:42 +0800

    feat(pie): 修复饼图引线穿透并新增对齐和外半径调节，升级版本至 1.0.2

--- charts/build.gradle.kts
+++ charts/build.gradle.kts
@@ -73,7 +73,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.aike1202"
                 artifactId = "charts"
-                version = "1.0.0"
+                version = "1.0.2"
             }
         }
     }
